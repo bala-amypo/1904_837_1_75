40422e010cde5f0e06ea77b8baa171d9194cea6c
bala-amypo <bala@amypo.in>
2025-12-19 16:04:23 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitLogService.java
+++ src/main/java/com/example/demo/service/VisitLogService.java
@@ -6,14 +6,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface VisitLogService {
-
     VisitLog createVisitLog(Long visitorId, VisitLog log);
-
-    VisitLog updateExit(Long logId);
-
     VisitLog getLog(Long id);
-
     List<VisitLog> getLogsByVisitor(Long visitorId);
-
-    List<VisitLog> getLogsSince(Long visitorId, LocalDateTime since);
 }
+
