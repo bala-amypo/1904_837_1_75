02042d7fe252ff6b6ded4373298dd5143687638f
bala-amypo <bala@amypo.in>
2025-12-29 05:58:50 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,36 +1,10 @@
-package com.example.demo.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import java.util.List;
-
-@Configuration
-public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://9292.408procr.amypo.ai/")
-                ));
-        }
-}
-
-
 // package com.example.demo.config;
 
-// import java.util.List;
-
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.servers.Server;
 // import org.springframework.context.annotation.Bean;
 // import org.springframework.context.annotation.Configuration;
-
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.Components;
-// import io.swagger.v3.oas.models.security.SecurityRequirement;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
-// import io.swagger.v3.oas.models.servers.Server;
+// import java.util.List;
 
 // @Configuration
 // public class OpenApiConfig {
@@ -38,22 +12,48 @@ public class OpenApiConfig {
 //     @Bean
 //     public OpenAPI customOpenAPI() {
 //         return new OpenAPI()
-
 //                 .servers(List.of(
 //                         new Server().url("https://9292.408procr.amypo.ai/")
-//                 ))
-
-//                 .components(new Components()
-//                         .addSecuritySchemes("bearerAuth",
-//                                 new SecurityScheme()
-//                                         .type(SecurityScheme.Type.HTTP)
-//                                         .scheme("bearer")
-//                                         .bearerFormat("JWT")
-//                         )
-//                 )
-
-//                 .addSecurityItem(
-//                         new SecurityRequirement().addList("bearerAuth")
-//                 );
-//     }
+//                 ));
+//         }
 // }
+
+
+package com.example.demo.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+
+                .servers(List.of(
+                        new Server().url("https://9292.408procr.amypo.ai/")
+                ))
+
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                );
+    }
+}
